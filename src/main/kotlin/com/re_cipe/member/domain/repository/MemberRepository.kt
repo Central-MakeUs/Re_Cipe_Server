220474e6408f6fe15da94faaab0e3d12882651e3
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-07-11 00:05:54 +0900

    feat: MemberRepository 생성

---
+++ src/main/kotlin/com/re_cipe/member/domain/repository/MemberRepository.kt
@@ -0,0 +1,7 @@
+package com.re_cipe.member.domain.repository
+
+import com.re_cipe.member.domain.Member
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MemberRepository : JpaRepository<Member, Long> {
+}
